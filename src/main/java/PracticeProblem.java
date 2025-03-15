af6e404e341439029e730514e027c02f1deaeea1
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-03-15 21:24:37 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String backwardsReadFile(String f) {
+		BufferedReader r = null;
+		Scanner s = null;
+		String output = "";
+		try {
+			r = new BufferedReader(new FileReader(f));
+			while(r.ready()){
+				output = (char) r.read() + output;
+			}
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+		return output;
+	}
+	public static String readFile(String f) {
+		BufferedReader r = null;
+		Scanner s = null;
+		String output = "";
+		try {
+			r = new BufferedReader(new FileReader(f));
+			while(r.ready()){
+				output = output + (char) r.read();
+			}
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+		return output;
+	}
 
 }
